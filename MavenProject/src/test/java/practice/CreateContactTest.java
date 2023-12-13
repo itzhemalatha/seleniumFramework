8ce39fa338f7860947ac8fc83c83595a4b78a96d
itzhe <itzhe@hemavijay>
2023-12-13 13:14:00 +0530

    1st push

---
+++ MavenProject/src/test/java/practice/CreateContactTest.java
@@ -0,0 +1,20 @@
+package practice;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateContactTest {
+@Test
+	public void Test1()
+	{
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("https://www.amazon.in");
+		System.out.println("hello");
+	}
+
+
+}
+
---
+++ MavenProject/src/test/java/practice/DemoTest.java
@@ -0,0 +1,37 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoTest {
+
+	
+	public void demoTest()
+	{
+	String BROWSER = System.getProperty("browser");
+	String URL = System.getProperty("url");
+	System.out.println("BROWSER");
+	System.out.println("URL");
+	
+	WebDriver driver;
+	
+	if(BROWSER.equalsIgnoreCase("chrome"))
+	{
+	 WebDriverManager.chromedriver().setup();
+	 driver=new ChromeDriver();
+	 
+	}
+	else {
+		driver=new EdgeDriver();
+	}
+	driver.get(URL);
+	
+	
+	
+	
+	}
+	
+}
